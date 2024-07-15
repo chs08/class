4625bbac8650d2039b2b27139e925d7355011175
chs08 <t86662998@gmail.com>
2024-07-16 08:21:52 +0900

    commit3

--- pages/quiz.py
+++ pages/quiz.py
@@ -3,16 +3,14 @@ import streamlit as st
 quiz = st.radio(
     "1+1",
     ["0", "1", "2","3","4"])
+
+if "answer" not in st.session_state:
+    st.session_state.answer = "-"
+
 if st.button('제출'):
     if quiz == "2":
-        if "answer" not in st.session_state:
-            st.session_state.answer = "정답"
-        else:
-            st.session_state.answer = "정답"
+        st.session_state.answer = "정답"
     else:
-        if "answer" not in st.session_state:
-            st.session_state.answer = "오답"
-        else:
-            st.session_state.answer = "오답"
-
+        st.session_state.answer = "오답"
+       
    
