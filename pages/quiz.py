7fe5ccc4af0e4513d9535a0aec0653d50eace896
chs08 <t86662998@gmail.com>
2024-07-16 08:11:16 +0900

    commit2

--- pages/quiz.py
+++ pages/quiz.py
@@ -6,8 +6,13 @@ quiz = st.radio(
 if st.button('제출'):
     if quiz == "2":
         if "answer" not in st.session_state:
-        st.session_state.answer = "정답"
+            st.session_state.answer = "정답"
+        else:
+            st.session_state.answer = "정답"
     else:
         if "answer" not in st.session_state:
-        st.session_state.answer = "오답"
+            st.session_state.answer = "오답"
+        else:
+            st.session_state.answer = "오답"
+
    
