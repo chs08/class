b73fb0f087864f8ffee45acbc616d9aacbef11bd
chs08 <t86662998@gmail.com>
2024-07-16 09:39:00 +0900

    commit2

--- pages/quiz.py
+++ pages/quiz.py
@@ -1,16 +1,28 @@
 import streamlit as st
 
+st.header('문제 1')
 quiz = st.radio(
     "1+1",
     ["0", "1", "2","3","4"])
 
+st.write('---')
+
+st.header('문제 2')
+st.write('5월 8일은 무슨날?')
+quiz2 = st.text_input('답을 입력')
+
 if "answer" not in st.session_state:
-    st.session_state.answer = "-"
+    st.session_state.answer = "답안을 제출"
+if "answer2" not in st.session_state:
+    st.session_state.answer2 = "답안을 제출"
 
 if st.button('제출'):
+    if quiz2 == '어버이날'or'어버이 날':
+        st.session_state.answer2 = "정답"
+    else:
+        st.session_state.answer = "오답"
     if quiz == "2":
         st.session_state.answer = "정답"
     else:
         st.session_state.answer = "오답"
-       
    
--- pages/result.py
+++ pages/result.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
+st.header('문제 1')
 st.write(st.session_state.answer)
+st.write('---')
+st.header('문제 2')
+st.write(st.session_state.answer2)
